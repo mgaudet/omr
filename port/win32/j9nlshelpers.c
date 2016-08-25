07b84af73fc6e23591d5bd885e364e11e9c8b7ce
Matthew Gaudet <magaudet@ca.ibm.com>
2016-08-25 16:22:37 -0400

    Reformatted port/win32 into llvm using clang-format-3.8
       .clang-format:
       BasedOnStyle: llvm\nSortIncludes: false

--- port/win32/j9nlshelpers.c
+++ port/win32/j9nlshelpers.c
@@ -13,7 +13,8 @@
  *      http://www.opensource.org/licenses/apache2.0.php
  *
  * Contributors:
- *    Multiple authors (IBM Corp.) - initial API and implementation and/or initial documentation
+ *    Multiple authors (IBM Corp.) - initial API and implementation and/or
+ *initial documentation
  *******************************************************************************/
 
 /**
@@ -21,7 +22,6 @@
  * @ingroup Port
  * @brief Native language support helpers
  */
-
 
 #include <windows.h>
 #include <winbase.h>
@@ -40,66 +40,62 @@ void nls_determine_locale(struct OMRPortLibrary *portLibrary);
  *
  * @param[in] portLibrary The port library
  */
-void
-nls_determine_locale(struct OMRPortLibrary *portLibrary)
-{
-	LCID localeID = LOCALE_USER_DEFAULT;
-	LCTYPE infoType;
-	int length;
+void nls_determine_locale(struct OMRPortLibrary *portLibrary) {
+  LCID localeID = LOCALE_USER_DEFAULT;
+  LCTYPE infoType;
+  int length;
 #ifdef UNICODE
-	int i;
+  int i;
 #endif
-	char lang[8];
-	char country[8];
-	J9NLSDataCache *nls = &portLibrary->portGlobals->nls_data;
+  char lang[8];
+  char country[8];
+  J9NLSDataCache *nls = &portLibrary->portGlobals->nls_data;
 
-	OMRPORT_ACCESS_FROM_OMRPORT(portLibrary);
+  OMRPORT_ACCESS_FROM_OMRPORT(portLibrary);
 
-	/* Get the language */
-	infoType = LOCALE_SISO639LANGNAME;
+  /* Get the language */
+  infoType = LOCALE_SISO639LANGNAME;
 
-	length = GetLocaleInfo(localeID, infoType, (LPTSTR)&lang[0], sizeof(lang));
-	if (length < 2) {
-		strncpy(nls->language, "en", 2);
-	} else {
+  length = GetLocaleInfo(localeID, infoType, (LPTSTR)&lang[0], sizeof(lang));
+  if (length < 2) {
+    strncpy(nls->language, "en", 2);
+  } else {
 
 #ifdef UNICODE
-		/* convert double byte to single byte */
-		for (i = 0; i < length; i++) {
-			lang[i] = (char)((short *)lang)[i];
-		}
+    /* convert double byte to single byte */
+    for (i = 0; i < length; i++) {
+      lang[i] = (char)((short *)lang)[i];
+    }
 #endif
 
-		_strlwr(lang);
+    _strlwr(lang);
 
-		if (!strcmp(lang, "jp")) {
-			// Not required for NT, Win32 gets it wrong
-			strncpy(nls->language, "ja", 2);
-		} else if (!strcmp(lang, "ch")) {
-			/*[PR 104204] Pocket PC 2004 gives ch instead of zh for Chinese */
-			strncpy(nls->language, "zh", 2);
-		} else {
-			strncpy(nls->language, lang, 3);
-		}
-	}
+    if (!strcmp(lang, "jp")) {
+      // Not required for NT, Win32 gets it wrong
+      strncpy(nls->language, "ja", 2);
+    } else if (!strcmp(lang, "ch")) {
+      /*[PR 104204] Pocket PC 2004 gives ch instead of zh for Chinese */
+      strncpy(nls->language, "zh", 2);
+    } else {
+      strncpy(nls->language, lang, 3);
+    }
+  }
 
-	/* Get the region */
-	infoType = LOCALE_SISO3166CTRYNAME;
+  /* Get the region */
+  infoType = LOCALE_SISO3166CTRYNAME;
 
-	length = GetLocaleInfo(localeID, infoType, (LPTSTR)&country[0], sizeof(country));
-	if (length < 2) {
-		strncpy(nls->region, "US", 2);
-	} else {
+  length =
+      GetLocaleInfo(localeID, infoType, (LPTSTR)&country[0], sizeof(country));
+  if (length < 2) {
+    strncpy(nls->region, "US", 2);
+  } else {
 #ifdef UNICODE
-		// convert double byte to single byte
-		for (i = 0; i < length; i++) {
-			country[i] = (char)((short *)country)[i];
-		}
+    // convert double byte to single byte
+    for (i = 0; i < length; i++) {
+      country[i] = (char)((short *)country)[i];
+    }
 #endif
-		country[2] = 0; /* force null-terminator */
-		strncpy(nls->region, country, 2);
-	}
-
+    country[2] = 0; /* force null-terminator */
+    strncpy(nls->region, country, 2);
+  }
 }
-
-
--- port/win32/omrcpu.c
+++ port/win32/omrcpu.c
@@ -13,7 +13,8 @@
  *      http://www.opensource.org/licenses/apache2.0.php
  *
  * Contributors:
- *    Multiple authors (IBM Corp.) - initial API and implementation and/or initial documentation
+ *    Multiple authors (IBM Corp.) - initial API and implementation and/or
+ *initial documentation
  *******************************************************************************/
 
 /**
@@ -24,45 +25,40 @@
  * Functions setting CPU attributes.
  */
 
-
 #include <windows.h>
 #include "omrport.h"
-
 
 /**
  * PortLibrary startup.
  *
- * This function is called during startup of the portLibrary.  Any resources that are required for
- * the exit operations may be created here.  All resources created here should be destroyed
+ * This function is called during startup of the portLibrary.  Any resources
+ * that are required for
+ * the exit operations may be created here.  All resources created here should
+ * be destroyed
  * in @ref omrcpu_shutdown.
  *
  * @param[in] portLibrary The port library
  *
- * @return 0 on success, negative error code on failure.  Error code values returned are
+ * @return 0 on success, negative error code on failure.  Error code values
+ * returned are
  * \arg OMRPORT_ERROR_STARTUP_CPU
  *
  * @note Most implementations will simply return success.
  */
-int32_t
-omrcpu_startup(struct OMRPortLibrary *portLibrary)
-{
-	return 0;
-}
+int32_t omrcpu_startup(struct OMRPortLibrary *portLibrary) { return 0; }
 
 /**
  * PortLibrary shutdown.
  *
- * This function is called during shutdown of the portLibrary.  Any resources that were created by @ref omrcpu_startup
+ * This function is called during shutdown of the portLibrary.  Any resources
+ * that were created by @ref omrcpu_startup
  * should be destroyed here.
  *
  * @param[in] portLibrary The port library
  *
  * @note Most implementations will be empty.
  */
-void
-omrcpu_shutdown(struct OMRPortLibrary *portLibrary)
-{
-}
+void omrcpu_shutdown(struct OMRPortLibrary *portLibrary) {}
 
 /**
  * @brief CPU Control operations.
@@ -73,16 +69,12 @@ omrcpu_shutdown(struct OMRPortLibrary *portLibrary)
  * @param[in] memoryPointer The base address of memory to flush.
  * @param[in] byteAmount Number of bytes to flush.
  */
-void
-omrcpu_flush_icache(struct OMRPortLibrary *portLibrary, void *memoryPointer, uintptr_t byteAmount)
-{
-	FlushInstructionCache(GetCurrentProcess(), memoryPointer, byteAmount);
+void omrcpu_flush_icache(struct OMRPortLibrary *portLibrary,
+                         void *memoryPointer, uintptr_t byteAmount) {
+  FlushInstructionCache(GetCurrentProcess(), memoryPointer, byteAmount);
 }
 
-int32_t
-omrcpu_get_cache_line_size(struct OMRPortLibrary *portLibrary, int32_t *lineSize)
-{
-	return OMRPORT_ERROR_NOT_SUPPORTED_ON_THIS_PLATFORM;
+int32_t omrcpu_get_cache_line_size(struct OMRPortLibrary *portLibrary,
+                                   int32_t *lineSize) {
+  return OMRPORT_ERROR_NOT_SUPPORTED_ON_THIS_PLATFORM;
 }
-
-

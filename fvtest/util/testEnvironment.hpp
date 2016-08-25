519b286686902c361714ba110f83ba914d4b470f
Matthew Gaudet <magaudet@ca.ibm.com>
2016-08-25 17:01:32 -0400

    Reformatted fvtest/util into webkit using clang-format-3.8
       .clang-format:
       BasedOnStyle: webkit\nSortIncludes: false

--- fvtest/util/testEnvironment.hpp
+++ fvtest/util/testEnvironment.hpp
@@ -16,7 +16,6 @@
  *    Multiple authors (IBM Corp.) - initial implementation and documentation
  *******************************************************************************/
 
-
 #ifndef TESTENVIRONMENT_HPP_
 #define TESTENVIRONMENT_HPP_
 
@@ -42,103 +41,104 @@
 #include "omrTest.h"
 
 enum LogLevel {
-	LEVEL_VERBOSE = 0,
-	LEVEL_INFO,
-	LEVEL_WARN,
-	LEVEL_ERROR,
-	LEVEL_OFF
+    LEVEL_VERBOSE = 0,
+    LEVEL_INFO,
+    LEVEL_WARN,
+    LEVEL_ERROR,
+    LEVEL_OFF
 };
 
 /**
  * BaseEnvironment provides access to command line arguments for all tests run from main().
  */
-class BaseEnvironment: public ::testing::Environment
-{
-	/*
+class BaseEnvironment : public ::testing::Environment {
+    /*
 	 * Data members
 	 */
 private:
 protected:
-	LogLevel _minLevel;
-public:
-	int _argc;
-	char **_argv;
-	OMRPortLibrary *portLib;
+    LogLevel _minLevel;
 
-	/*
+public:
+    int _argc;
+    char** _argv;
+    OMRPortLibrary* portLib;
+
+    /*
 	 * Function members
 	 */
 private:
 protected:
-	virtual void
-	SetUp()
-	{
-		for (int i = 1; i < _argc; i++) {
-			if (0 == strncmp(_argv[i], "-logLevel=", strlen("-logLevel="))) {
-				const char *logLevelStr = &_argv[i][strlen("-logLevel=")];
-				if (0 == strcmp(logLevelStr, "verbose")) {
-					_minLevel = LEVEL_VERBOSE;
-				} else if (0 == strcmp(logLevelStr, "info")) {
-					_minLevel = LEVEL_INFO;
-				} else if (0 == strcmp(logLevelStr, "warn")) {
-					_minLevel = LEVEL_WARN;
-				} else if (0 == strcmp(logLevelStr, "error")) {
-					_minLevel = LEVEL_ERROR;
-				} else if (0 == strcmp(logLevelStr, "off")) {
-					_minLevel = LEVEL_OFF;
-				}	else {
-					FAIL() << "Unrecognized -logLevel option, the valid log levels are verbose, info, warn, error, off.";
-				}
-			}
-		}
-	}
+    virtual void
+    SetUp()
+    {
+        for (int i = 1; i < _argc; i++) {
+            if (0 == strncmp(_argv[i], "-logLevel=", strlen("-logLevel="))) {
+                const char* logLevelStr = &_argv[i][strlen("-logLevel=")];
+                if (0 == strcmp(logLevelStr, "verbose")) {
+                    _minLevel = LEVEL_VERBOSE;
+                } else if (0 == strcmp(logLevelStr, "info")) {
+                    _minLevel = LEVEL_INFO;
+                } else if (0 == strcmp(logLevelStr, "warn")) {
+                    _minLevel = LEVEL_WARN;
+                } else if (0 == strcmp(logLevelStr, "error")) {
+                    _minLevel = LEVEL_ERROR;
+                } else if (0 == strcmp(logLevelStr, "off")) {
+                    _minLevel = LEVEL_OFF;
+                } else {
+                    FAIL() << "Unrecognized -logLevel option, the valid log levels are verbose, info, warn, error, off.";
+                }
+            }
+        }
+    }
 
-	virtual void TearDown() { }
+    virtual void TearDown() {}
 
-	virtual void log_vprintf(LogLevel ll, const char *format, va_list args) {
-		if (NULL != portLib) {
-			OMRPORT_ACCESS_FROM_OMRPORT(portLib);
-			if (ll >= _minLevel) {
-				omrtty_vprintf(format, args);
-			}
-		} else {
-			FAIL() << "Log function can only be used by child of BaseEnvironment which has port library initialized!";
-		}
-	}
+    virtual void log_vprintf(LogLevel ll, const char* format, va_list args)
+    {
+        if (NULL != portLib) {
+            OMRPORT_ACCESS_FROM_OMRPORT(portLib);
+            if (ll >= _minLevel) {
+                omrtty_vprintf(format, args);
+            }
+        } else {
+            FAIL() << "Log function can only be used by child of BaseEnvironment which has port library initialized!";
+        }
+    }
 
 public:
-	BaseEnvironment(int argc, char **argv)
-		: ::testing::Environment()
-		, _minLevel(LEVEL_INFO)
-		, _argc(argc)
-		, _argv(argv)
-		, portLib(NULL)
-	{
-	}
+    BaseEnvironment(int argc, char** argv)
+        : ::testing::Environment()
+        , _minLevel(LEVEL_INFO)
+        , _argc(argc)
+        , _argv(argv)
+        , portLib(NULL)
+    {
+    }
 
-	OMRPortLibrary *
-	getPortLibrary()
-	{
-		return portLib;
-	}
+    OMRPortLibrary*
+    getPortLibrary()
+    {
+        return portLib;
+    }
 
-	virtual void
-	log(const char *format, ...)
-	{
-		va_list args;
-		va_start(args, format);
-		log_vprintf(LEVEL_INFO, format, args);
-		va_end(args);
-	}
-	
-	virtual void
-	log(LogLevel ll, const char *format, ...)
-	{
-		va_list args;
-		va_start(args, format);
-		log_vprintf(ll, format, args);
-		va_end(args);
-	}
+    virtual void
+    log(const char* format, ...)
+    {
+        va_list args;
+        va_start(args, format);
+        log_vprintf(LEVEL_INFO, format, args);
+        va_end(args);
+    }
+
+    virtual void
+    log(LogLevel ll, const char* format, ...)
+    {
+        va_list args;
+        va_start(args, format);
+        log_vprintf(ll, format, args);
+        va_end(args);
+    }
 };
 
 /*
@@ -150,16 +150,15 @@ public:
  * Note that Googletest's ASSERT_*() macros cannot be used in this context. Those macros can
  * only be used in methods that return void.
  */
-#define ATTACH_J9THREAD() \
-	do { \
-		intptr_t rc = omrthread_attach_ex(NULL, J9THREAD_ATTR_DEFAULT); \
-		if (0 != rc) { \
-			fprintf(stderr, "omrthread_attach_ex(NULL, J9THREAD_ATTR_DEFAULT) failed, rc=%d\n", (int)rc); \
-		} \
-	} while (0)
+#define ATTACH_J9THREAD()                                                                                 \
+    do {                                                                                                  \
+        intptr_t rc = omrthread_attach_ex(NULL, J9THREAD_ATTR_DEFAULT);                                   \
+        if (0 != rc) {                                                                                    \
+            fprintf(stderr, "omrthread_attach_ex(NULL, J9THREAD_ATTR_DEFAULT) failed, rc=%d\n", (int)rc); \
+        }                                                                                                 \
+    } while (0)
 
 #define DETACH_J9THREAD() omrthread_detach(NULL)
-
 
 /**
  * PortEnvironment provides BaseEnvironment and initializes/finalizes the j9port
@@ -169,46 +168,47 @@ public:
  * down after RUN_ALL_TESTS(). The ATTACH_J9THREAD() and DETACH_J9THREAD() macros can be used
  * for this purpose.
  */
-class PortEnvironment: public BaseEnvironment
-{
-	/*
+class PortEnvironment : public BaseEnvironment {
+    /*
 	 * Data members
 	 */
 private:
-	OMRPortLibrary _portLibrary;
+    OMRPortLibrary _portLibrary;
+
 protected:
 public:
-
-	/*
+    /*
 	 * Function members
 	 */
 private:
 protected:
-	virtual void
-	SetUp()
-	{
-		BaseEnvironment::SetUp();
+    virtual void
+    SetUp()
+    {
+        BaseEnvironment::SetUp();
 
-		/* Use portlibrary version which we compiled against, and have allocated space
+        /* Use portlibrary version which we compiled against, and have allocated space
 		 * for on the stack.  This version may be different from the one in the linked DLL.
 		 */
-		int rc = (int)omrport_init_library(&_portLibrary, sizeof(OMRPortLibrary));
-		ASSERT_EQ(0, rc) << "omrport_init_library(&_OMRPortLibrary, sizeof(OMRPortLibrary)) failed, %d\n" << rc;
-		portLib = &_portLibrary;
-	}
+        int rc = (int)omrport_init_library(&_portLibrary, sizeof(OMRPortLibrary));
+        ASSERT_EQ(0, rc) << "omrport_init_library(&_OMRPortLibrary, sizeof(OMRPortLibrary)) failed, %d\n"
+                         << rc;
+        portLib = &_portLibrary;
+    }
 
-	virtual void
-	TearDown()
-	{
-		_portLibrary.port_shutdown_library(&_portLibrary);
+    virtual void
+    TearDown()
+    {
+        _portLibrary.port_shutdown_library(&_portLibrary);
 
-		BaseEnvironment::TearDown();
-	}
+        BaseEnvironment::TearDown();
+    }
+
 public:
-	PortEnvironment(int argc, char **argv)
-		: BaseEnvironment(argc, argv)
-	{
-	}
+    PortEnvironment(int argc, char** argv)
+        : BaseEnvironment(argc, argv)
+    {
+    }
 };
 
 #endif /* TESTENVIRONMENT_HPP_ */

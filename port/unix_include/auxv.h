d30725c74ca95ed806e3fd28ee1b7ba202ca8743
Matthew Gaudet <magaudet@ca.ibm.com>
2016-08-25 17:01:36 -0400

    Reformatted port/unix_include into webkit using clang-format-3.8
       .clang-format:
       BasedOnStyle: webkit\nSortIncludes: false

--- port/unix_include/auxv.h
+++ port/unix_include/auxv.h
@@ -29,14 +29,14 @@
 /* We aren't going to assume that the system elf.h has the latest information
  * on all AUXV additions.  */
 #ifndef AT_BASE_PLATFORM
-#define AT_BASE_PLATFORM 24		/* String identifying real platforms.*/
+#define AT_BASE_PLATFORM 24 /* String identifying real platforms.*/
 #endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
-  /* Function: prefetch_auxv
+/* Function: prefetch_auxv
    * -------------------------------------------------------------------------
    *
    *  The aux vector will be loaded into application addressable memory.
@@ -63,9 +63,9 @@ extern "C" {
    *   Note: On failure, no memory is allocated.
    *
    */
-  extern int prefetch_auxv(void);
+extern int prefetch_auxv(void);
 
-  /* Function: get_auxv
+/* Function: get_auxv
    * -------------------------------------------------------------------------
    *
    *  Returns a pointer to the aux vector that has been read into memory.
@@ -76,9 +76,9 @@ extern "C" {
    *  return the address of that memory.  If memory has not been allocated it
    *  will return zero.
    */
-  extern ElfW(auxv_t) * get_auxv(void);
+extern ElfW(auxv_t) * get_auxv(void);
 
-  /* Function: query_auxv
+/* Function: query_auxv
    * -------------------------------------------------------------------------
    *
    *  Queries the aux vector for an auxv_t struct where that auxv_t
@@ -106,7 +106,7 @@ extern "C" {
    *
    */
 
-  extern ElfW(Addr) query_auxv(ElfW(Addr));
+extern ElfW(Addr) query_auxv(ElfW(Addr));
 
 #ifdef __cplusplus
 }

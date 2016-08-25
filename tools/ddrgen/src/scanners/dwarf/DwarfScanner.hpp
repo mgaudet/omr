96f451f5d490d7e00a60a5a4f7377678c1692ec9
Matthew Gaudet <magaudet@ca.ibm.com>
2016-08-25 16:43:24 -0400

    Reformatted tools/ddrgen/src/scanners/dwarf into Mozilla using clang-format-3.8
       .clang-format:
       BasedOnStyle: Mozilla\nSortIncludes: false

--- tools/ddrgen/src/scanners/dwarf/DwarfScanner.hpp
+++ tools/ddrgen/src/scanners/dwarf/DwarfScanner.hpp
@@ -41,69 +41,79 @@ using std::tr1::unordered_map;
 
 #if defined(AIXPPC)
 using std::tr1::hash;
-#else /* defined(AIXPPC) */
+#else  /* defined(AIXPPC) */
 using std::hash;
 #endif /* !defined(AIXPPC) */
 using std::map;
 using std::string;
 #if defined(AIXPPC)
 using std::tr1::unordered_map;
-#else /* defined(AIXPPC) */
+#else  /* defined(AIXPPC) */
 using std::unordered_map;
 #endif /* !defined(AIXPPC) */
 
-struct TypeKey {
-	char *fileName;
-	string typeName;
-	int lineNumber;
+struct TypeKey
+{
+  char* fileName;
+  string typeName;
+  int lineNumber;
 
-	bool operator==(const TypeKey& other) const;
+  bool operator==(const TypeKey& other) const;
 };
 
-struct KeyHasher {
-	size_t operator()(const TypeKey& key) const;
+struct KeyHasher
+{
+  size_t operator()(const TypeKey& key) const;
 };
 
-class DwarfScanner: public Scanner
+class DwarfScanner : public Scanner
 {
 public:
-	DwarfScanner();
-	~DwarfScanner();
+  DwarfScanner();
+  ~DwarfScanner();
 
-	DDR_RC startScan(OMRPortLibrary *portLibrary, Symbol_IR *const ir, vector<string> *debugFiles);
+  DDR_RC startScan(OMRPortLibrary* portLibrary, Symbol_IR* const ir,
+                   vector<string>* debugFiles);
 
-	DDR_RC dispatchScanChildInfo(Type *newType, void *data);
-	DDR_RC dispatchScanChildInfo(NamespaceUDT *newClass, void *data);
-	DDR_RC dispatchScanChildInfo(EnumUDT *newEnum, void *data);
-	DDR_RC dispatchScanChildInfo(TypedefUDT *newTypedef, void *data);
-	DDR_RC dispatchScanChildInfo(ClassUDT *newTypedef, void *data);
-	DDR_RC dispatchScanChildInfo(UnionUDT *newTypedef, void *data);
+  DDR_RC dispatchScanChildInfo(Type* newType, void* data);
+  DDR_RC dispatchScanChildInfo(NamespaceUDT* newClass, void* data);
+  DDR_RC dispatchScanChildInfo(EnumUDT* newEnum, void* data);
+  DDR_RC dispatchScanChildInfo(TypedefUDT* newTypedef, void* data);
+  DDR_RC dispatchScanChildInfo(ClassUDT* newTypedef, void* data);
+  DDR_RC dispatchScanChildInfo(UnionUDT* newTypedef, void* data);
 
 private:
-	Dwarf_Signed _fileNameCount;
-	char **_fileNamesTable;
-	unordered_map<TypeKey, Type *, KeyHasher> _typeMap;
-	unordered_map<string, Type *> _typeStubMap;
-	Symbol_IR *_ir;
-	Dwarf_Debug _debug;
+  Dwarf_Signed _fileNameCount;
+  char** _fileNamesTable;
+  unordered_map<TypeKey, Type*, KeyHasher> _typeMap;
+  unordered_map<string, Type*> _typeStubMap;
+  Symbol_IR* _ir;
+  Dwarf_Debug _debug;
 
-	DDR_RC scanFile(OMRPortLibrary *portLibrary, Symbol_IR *ir, const char *filepath);
-	DDR_RC traverse_cu_in_debug_section(Symbol_IR *const ir);
-	DDR_RC addType(Dwarf_Die die, Dwarf_Half tag, bool ignoreFilter, bool isSubUDT, Type **type);
-	DDR_RC getType(Dwarf_Die die, Dwarf_Half tag, Type **const newUDT, bool isSubUDT, int *typeNum);
-	DDR_RC createType(Dwarf_Die die, Dwarf_Half tag, string dieName, unsigned int lineNumber, Type **const newUDT);
-	DDR_RC addEnumMember(Dwarf_Die die, EnumUDT *const udt);
-	DDR_RC addClassField(Dwarf_Die die, ClassType *const newClass, string fieldName);
-	DDR_RC getSuperUDT(Dwarf_Die die, ClassUDT *const udt);
-	DDR_RC getTypeInfo(Dwarf_Die die, Dwarf_Die *dieout, string *typeName, Modifiers *modifiers, size_t *typeSize, size_t *bitField);
-	DDR_RC getTypeSize(Dwarf_Die die, size_t *typeSize);
-	DDR_RC getTypeTag(Dwarf_Die die, Dwarf_Die *typedie, Dwarf_Half *tag);
-	DDR_RC getBlacklist(Dwarf_Die die);
-	DDR_RC blackListedDie(Dwarf_Die die, bool *dieBlackListed);
-	DDR_RC getName(Dwarf_Die die, string *name);
-	DDR_RC createTypeKey(Dwarf_Die die, string typeName, TypeKey *key, unsigned int *lineNumber, string *fileName);
-	DDR_RC getNextSibling(Dwarf_Die *die);
-	DDR_RC getBitField(Dwarf_Die die, size_t *bitField);
+  DDR_RC scanFile(OMRPortLibrary* portLibrary, Symbol_IR* ir,
+                  const char* filepath);
+  DDR_RC traverse_cu_in_debug_section(Symbol_IR* const ir);
+  DDR_RC addType(Dwarf_Die die, Dwarf_Half tag, bool ignoreFilter,
+                 bool isSubUDT, Type** type);
+  DDR_RC getType(Dwarf_Die die, Dwarf_Half tag, Type** const newUDT,
+                 bool isSubUDT, int* typeNum);
+  DDR_RC createType(Dwarf_Die die, Dwarf_Half tag, string dieName,
+                    unsigned int lineNumber, Type** const newUDT);
+  DDR_RC addEnumMember(Dwarf_Die die, EnumUDT* const udt);
+  DDR_RC addClassField(Dwarf_Die die, ClassType* const newClass,
+                       string fieldName);
+  DDR_RC getSuperUDT(Dwarf_Die die, ClassUDT* const udt);
+  DDR_RC getTypeInfo(Dwarf_Die die, Dwarf_Die* dieout, string* typeName,
+                     Modifiers* modifiers, size_t* typeSize, size_t* bitField);
+  DDR_RC getTypeSize(Dwarf_Die die, size_t* typeSize);
+  DDR_RC getTypeTag(Dwarf_Die die, Dwarf_Die* typedie, Dwarf_Half* tag);
+  DDR_RC getBlacklist(Dwarf_Die die);
+  DDR_RC blackListedDie(Dwarf_Die die, bool* dieBlackListed);
+  DDR_RC getName(Dwarf_Die die, string* name);
+  DDR_RC createTypeKey(Dwarf_Die die, string typeName, TypeKey* key,
+                       unsigned int* lineNumber, string* fileName);
+  DDR_RC getNextSibling(Dwarf_Die* die);
+  DDR_RC getBitField(Dwarf_Die die, size_t* bitField);
 };
 
 #endif /* DWARFSCANNER_HPP */

02ffbaa95b70f8fc8247f9a6ea4cd1d78b99fe14
Matthew Gaudet <magaudet@ca.ibm.com>
2016-08-25 16:43:23 -0400

    Reformatted port/win32 into Mozilla using clang-format-3.8
       .clang-format:
       BasedOnStyle: Mozilla\nSortIncludes: false

--- port/win32/omrmem.c
+++ port/win32/omrmem.c
@@ -13,7 +13,8 @@
  *      http://www.opensource.org/licenses/apache2.0.php
  *
  * Contributors:
- *    Multiple authors (IBM Corp.) - initial API and implementation and/or initial documentation
+ *    Multiple authors (IBM Corp.) - initial API and implementation and/or
+ *initial documentation
  *******************************************************************************/
 
 /**
@@ -21,7 +22,6 @@
  * @ingroup Port
  * @brief Memory Utilities
  */
-
 
 /*
  * This file contains code for the portability library memory management.
@@ -33,64 +33,71 @@
 #include "omrportpg.h"
 #include "ut_omrport.h"
 
-void *
-omrmem_allocate_memory_basic(struct OMRPortLibrary *portLibrary, uintptr_t byteAmount)
+void*
+omrmem_allocate_memory_basic(struct OMRPortLibrary* portLibrary,
+                             uintptr_t byteAmount)
 {
-	return HeapAlloc(PPG_mem_heap, 0, byteAmount);
+  return HeapAlloc(PPG_mem_heap, 0, byteAmount);
 }
 
 void
-omrmem_free_memory_basic(struct OMRPortLibrary *portLibrary, void *memoryPointer)
+omrmem_free_memory_basic(struct OMRPortLibrary* portLibrary,
+                         void* memoryPointer)
 {
-	HeapFree(PPG_mem_heap, 0, memoryPointer);
+  HeapFree(PPG_mem_heap, 0, memoryPointer);
 }
 
 void
-omrmem_advise_and_free_memory_basic(struct OMRPortLibrary *portLibrary, void *memoryPointer, uintptr_t memorySize)
+omrmem_advise_and_free_memory_basic(struct OMRPortLibrary* portLibrary,
+                                    void* memoryPointer, uintptr_t memorySize)
 {
-	omrmem_free_memory_basic(portLibrary, memoryPointer);
+  omrmem_free_memory_basic(portLibrary, memoryPointer);
 }
 
-void *
-omrmem_reallocate_memory_basic(struct OMRPortLibrary *portLibrary, void *memoryPointer, uintptr_t byteAmount)
+void*
+omrmem_reallocate_memory_basic(struct OMRPortLibrary* portLibrary,
+                               void* memoryPointer, uintptr_t byteAmount)
 {
-	return HeapReAlloc(PPG_mem_heap, 0, memoryPointer, byteAmount);
-}
-
-void
-omrmem_shutdown_basic(struct OMRPortLibrary *portLibrary)
-{
-	HANDLE memHeap = GetProcessHeap();
-
-	/* has to be cleaned up with HeapFree since omrmem_startup allocated it with HeapAlloc */
-	HeapFree(memHeap, 0, portLibrary->portGlobals);
+  return HeapReAlloc(PPG_mem_heap, 0, memoryPointer, byteAmount);
 }
 
 void
-omrmem_startup_basic(struct OMRPortLibrary *portLibrary, uintptr_t portGlobalSize)
+omrmem_shutdown_basic(struct OMRPortLibrary* portLibrary)
 {
-	HANDLE memHeap = GetProcessHeap();
+  HANDLE memHeap = GetProcessHeap();
 
-	/* done as a HeapAlloc because omrmem_allocate_memory requires portGlobals to be initialized */
-	portLibrary->portGlobals = HeapAlloc(memHeap, 0, portGlobalSize);
-	if (NULL == portLibrary->portGlobals) {
-		return;
-	}
-	memset(portLibrary->portGlobals, 0, portGlobalSize);
-
-	PPG_mem_heap = memHeap;
+  /* has to be cleaned up with HeapFree since omrmem_startup allocated it with
+   * HeapAlloc */
+  HeapFree(memHeap, 0, portLibrary->portGlobals);
 }
 
-void *
+void
+omrmem_startup_basic(struct OMRPortLibrary* portLibrary,
+                     uintptr_t portGlobalSize)
+{
+  HANDLE memHeap = GetProcessHeap();
+
+  /* done as a HeapAlloc because omrmem_allocate_memory requires portGlobals to
+   * be initialized */
+  portLibrary->portGlobals = HeapAlloc(memHeap, 0, portGlobalSize);
+  if (NULL == portLibrary->portGlobals) {
+    return;
+  }
+  memset(portLibrary->portGlobals, 0, portGlobalSize);
+
+  PPG_mem_heap = memHeap;
+}
+
+void*
 omrmem_allocate_portLibrary_basic(uintptr_t byteAmount)
 {
-	HANDLE memHeap = GetProcessHeap();
-	return HeapAlloc(memHeap, 0, byteAmount);
+  HANDLE memHeap = GetProcessHeap();
+  return HeapAlloc(memHeap, 0, byteAmount);
 }
 
 void
-omrmem_deallocate_portLibrary_basic(void *memoryPointer)
+omrmem_deallocate_portLibrary_basic(void* memoryPointer)
 {
-	HANDLE memHeap = GetProcessHeap();
-	HeapFree(memHeap, 0, memoryPointer);
+  HANDLE memHeap = GetProcessHeap();
+  HeapFree(memHeap, 0, memoryPointer);
 }

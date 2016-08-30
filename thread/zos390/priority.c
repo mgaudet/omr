28b4996527aadb50befd3aa8fd13a9a75129ac5f
Matthew Gaudet <magaudet@ca.ibm.com>
2016-08-30 16:26:34 -0400

    Reformatted thread/zos390 into Google using clang-format-3.8
       .clang-format:
       BasedOnStyle: Google\nSortIncludes: false

--- thread/zos390/priority.c
+++ thread/zos390/priority.c
@@ -18,51 +18,41 @@
 
 #include "thrdsup.h"
 
-
 #if defined(J9_PRIORITY_MAP)
 const int priority_map[J9THREAD_PRIORITY_MAX + 1] = J9_PRIORITY_MAP;
 #else
 int priority_map[J9THREAD_PRIORITY_MAX + 1];
-intptr_t
-initialize_priority_map(void)
-{
-	int i = 0;
-	for (i = 0; i < (J9THREAD_PRIORITY_MAX + 1); i++) {
-		priority_map[i] = 0;
-	}
+intptr_t initialize_priority_map(void) {
+  int i = 0;
+  for (i = 0; i < (J9THREAD_PRIORITY_MAX + 1); i++) {
+    priority_map[i] = 0;
+  }
 
-	return 0;
+  return 0;
 }
 #endif /* defined(J9_PRIORITY_MAP) */
 
-
-intptr_t
-set_pthread_priority(OSTHREAD handle, omrthread_prio_t j9ThreadPriority)
-{
-	/* Do nothing for now on z/OS 390 */
-	return 1;
+intptr_t set_pthread_priority(OSTHREAD handle,
+                              omrthread_prio_t j9ThreadPriority) {
+  /* Do nothing for now on z/OS 390 */
+  return 1;
 }
-void
-initialize_thread_priority(omrthread_t thread)
-{
-	int policy, priority, i;
+void initialize_thread_priority(omrthread_t thread) {
+  int policy, priority, i;
 
-	/* set the default value */
-	thread->priority = J9THREAD_PRIORITY_NORMAL;
+  /* set the default value */
+  thread->priority = J9THREAD_PRIORITY_NORMAL;
 
-	/* are we using priorities at all? */
-	if (priority_map[J9THREAD_PRIORITY_MIN] == priority_map[J9THREAD_PRIORITY_MAX]) {
-		return;
-	}
+  /* are we using priorities at all? */
+  if (priority_map[J9THREAD_PRIORITY_MIN] ==
+      priority_map[J9THREAD_PRIORITY_MAX]) {
+    return;
+  }
 
-	return;
+  return;
 }
 
-intptr_t
-set_priority_spread(void)
-{
-	/* This function should not be called on this platform */
-	return -1;
+intptr_t set_priority_spread(void) {
+  /* This function should not be called on this platform */
+  return -1;
 }
-
-

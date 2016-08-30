5f8fab040ae89d726d31fda12c6ccf6d5adf6142
Matthew Gaudet <magaudet@ca.ibm.com>
2016-08-30 16:25:36 -0400

    Reformatted gc/include into Chromium using clang-format-3.8
       .clang-format:
       BasedOnStyle: Chromium\nSortIncludes: false

--- gc/include/omrmodroncore.h
+++ gc/include/omrmodroncore.h
@@ -28,15 +28,18 @@
 
 /**
  * Card state definitions
- * @note CARD_DIRTY 0x01 has been hardcoded in builder see J9VMObjectAccessBarrier>>#dirtyCard
+ * @note CARD_DIRTY 0x01 has been hardcoded in builder see
+ * J9VMObjectAccessBarrier>>#dirtyCard
  * @ingroup GC_Include
  * @{
  */
 typedef U_8 Card;
 
 enum {
-	CARD_SIZE_SHIFT	= 9,	/* base2 log of CARD_SIZE, used to change division into shift */
-	CARD_SIZE = 512	/* size of a "card" of the heap - in a sense:  the granule of deferred mark map update */
+  CARD_SIZE_SHIFT =
+      9, /* base2 log of CARD_SIZE, used to change division into shift */
+  CARD_SIZE = 512 /* size of a "card" of the heap - in a sense:  the granule of
+                     deferred mark map update */
 };
 
 /*
@@ -44,21 +47,36 @@ enum {
  * Both versions of each definition must match exactly.
  */
 
-/* means that this card requires no special handling in the next collection as a result of previous collection or mutator activity */
+/* means that this card requires no special handling in the next collection as a
+ * result of previous collection or mutator activity */
 #define CARD_CLEAN 0x00
-/* means that this card has been changed by the mutator since the collector last processed it */
+/* means that this card has been changed by the mutator since the collector last
+ * processed it */
 #define CARD_DIRTY 0x01
-/* means that a PGC has viewed the card but the next GMP increment (or SYS/OOM fall-back) must also process the card (since it could have hidden objects) */
+/* means that a PGC has viewed the card but the next GMP increment (or SYS/OOM
+ * fall-back) must also process the card (since it could have hidden objects) */
 #define CARD_GMP_MUST_SCAN 0x02
-/* means that a GMP has viewed the card but the next PGC increment must also process the card (since it might have unseen inter-region references - the GMP has a smaller live set so it wouldn't see them nor does it update the RSLists) */
+/* means that a GMP has viewed the card but the next PGC increment must also
+ * process the card (since it might have unseen inter-region references - the
+ * GMP has a smaller live set so it wouldn't see them nor does it update the
+ * RSLists) */
 #define CARD_PGC_MUST_SCAN 0x03
-/* largely equivalent to CARD_PGC_MUST_SCAN but can only exist between RSCL flushing and the end of PGC mark and has the special connotation that the only objects, in this card, which refer to other regions have their remembered bit set */
+/* largely equivalent to CARD_PGC_MUST_SCAN but can only exist between RSCL
+ * flushing and the end of PGC mark and has the special connotation that the
+ * only objects, in this card, which refer to other regions have their
+ * remembered bit set */
 #define CARD_REMEMBERED 0x04
-/* largely equivalent to CARD_DIRTY but can only exist between RSCL flushing and the end of PGC mark and has the special connotation that the only objects, in this card, which refer to other regions have their remembered bit set */
+/* largely equivalent to CARD_DIRTY but can only exist between RSCL flushing and
+ * the end of PGC mark and has the special connotation that the only objects, in
+ * this card, which refer to other regions have their remembered bit set */
 #define CARD_REMEMBERED_AND_GMP_SCAN 0x05
-/* used to state that this card contains objects which refer to other regions but all of those who do have their remembered bit set and are not yet in the region's RSCL - the compact treats this much like PGC_MUST_SCAN in that it may promote it to GMP_MUST_SCAN if a GMP is active */
+/* used to state that this card contains objects which refer to other regions
+ * but all of those who do have their remembered bit set and are not yet in the
+ * region's RSCL - the compact treats this much like PGC_MUST_SCAN in that it
+ * may promote it to GMP_MUST_SCAN if a GMP is active */
 #define CARD_MARK_COMPACT_TRANSITION 0x06
-/* used when a value must be passed which is considered uninitialized or otherwise invalid */
+/* used when a value must be passed which is considered uninitialized or
+ * otherwise invalid */
 #define CARD_INVALID 0xFF
 
 /**
@@ -68,9 +86,11 @@ enum {
 /**
  * The following definitions are duplicated in j9modron.h.
  * Both versions of each definition must match exactly.
- * 
- * #defines representing the vmState that should be set during various GC activities
- * @note J9VMSTATE_GC is the "major mask" representing a "GC" activity - we OR in a minor mask
+ *
+ * #defines representing the vmState that should be set during various GC
+ * activities
+ * @note J9VMSTATE_GC is the "major mask" representing a "GC" activity - we OR
+ * in a minor mask
  * representing the specific activity.
  * @ingroup GC_Include
  * @{
@@ -85,7 +105,8 @@ enum {
 #define J9VMSTATE_GC_CONCURRENT_MARK_SCAN_REMEMBERED_SET (J9VMSTATE_GC | 0x0009)
 #define J9VMSTATE_GC_CONCURRENT_MARK_FINAL_CLEAN_CARDS (J9VMSTATE_GC | 0x000A)
 #define J9VMSTATE_GC_CONCURRENT_SWEEP_COMPLETE_SWEEP (J9VMSTATE_GC | 0x000B)
-#define J9VMSTATE_GC_CONCURRENT_SWEEP_FIND_MINIMUM_SIZE_FREE (J9VMSTATE_GC | 0x000C)
+#define J9VMSTATE_GC_CONCURRENT_SWEEP_FIND_MINIMUM_SIZE_FREE \
+  (J9VMSTATE_GC | 0x000C)
 #define J9VMSTATE_GC_SCAVENGE (J9VMSTATE_GC | 0x000F)
 #define J9VMSTATE_GC_CONCURRENT_MARK_TRACE (J9VMSTATE_GC | 0x0016)
 #define J9VMSTATE_GC_CONCURRENT_SWEEP (J9VMSTATE_GC | 0x0017)

9d6af2533a59db8af10e6bdee439ec1e2c09c716
Matthew Gaudet <magaudet@ca.ibm.com>
2016-08-30 16:25:34 -0400

    Reformatted fvtest/algotest into Chromium using clang-format-3.8
       .clang-format:
       BasedOnStyle: Chromium\nSortIncludes: false

--- fvtest/algotest/main.cpp
+++ fvtest/algotest/main.cpp
@@ -21,19 +21,18 @@
 #include "testEnvironment.hpp"
 
 extern "C" {
-int testMain(int argc, char **argv, char **envp);
+int testMain(int argc, char** argv, char** envp);
 }
 
-PortEnvironment *omrTestEnv;
+PortEnvironment* omrTestEnv;
 
-int
-testMain(int argc, char **argv, char **envp)
-{
-	::testing::InitGoogleTest(&argc, argv);
+int testMain(int argc, char** argv, char** envp) {
+  ::testing::InitGoogleTest(&argc, argv);
 
-	ATTACH_J9THREAD();
-	omrTestEnv = (PortEnvironment *)testing::AddGlobalTestEnvironment(new PortEnvironment(argc, argv));
-	int result = RUN_ALL_TESTS();
-	DETACH_J9THREAD();
-	return result;
+  ATTACH_J9THREAD();
+  omrTestEnv = (PortEnvironment*)testing::AddGlobalTestEnvironment(
+      new PortEnvironment(argc, argv));
+  int result = RUN_ALL_TESTS();
+  DETACH_J9THREAD();
+  return result;
 }
